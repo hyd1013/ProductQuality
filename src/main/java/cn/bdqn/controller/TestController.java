8103b25c26fcec269f0a2709e213966f14287384
hyd1013 <huangyaodong@xmbdqn.com>
2022-11-10 15:44:54 +0800

    TestController第二次修改

--- src/main/java/cn/bdqn/controller/TestController.java
+++ src/main/java/cn/bdqn/controller/TestController.java
@@ -9,7 +9,7 @@ package cn.bdqn.controller;
 public class TestController {
 
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
+        System.out.println("hehehe");
     }
 
 }
